bc50454736a1d56bd2bcfe50fac3188da49bd7c9
Delroy van der Kemp <dbakemp@avans.nl>
2017-10-27 22:42:50 +0200

    infinite scroll v1

---
+++ Assets/Scrips/GameController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using TerrainGenerator;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    private const int Radius = 1;
+
+    private Vector2i PreviousPlayerChunkPosition;
+
+    public Transform Player;
+
+    public TerrainChunkGenerator Generator;
+    
+}
---
+++ Assets/Scrips/Generator/Chunk/TerrainChunkGenerator.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace TerrainGenerator
+{
+    public class TerrainChunkGenerator : MonoBehaviour
+    {
+        public Material TerrainMaterial;
+
+        public Texture2D FlatTexture;
+        public Texture2D SteepTexture;
+
+        private TerrainChunkSettings Settings;
+
+        private NoiseProvider NoiseProvider;
+
+        List<TerrainChunk> chunks = new List<TerrainChunk>();
+        
+        TerrainChunk newestChunk;
+
+        int xScrollPosition = 0;
+        int newScrollCounter = 0;
+
+        private void Awake()
+        {
+            Settings = new TerrainChunkSettings(129, 129, 100, 30, FlatTexture, SteepTexture, TerrainMaterial);
+            NoiseProvider = new NoiseProvider();
+
+            addChunk(0, 0);
+            addChunk(0, 1);
+            addChunk(0, 2);
+            addChunk(0, 3);
+            addChunk(0, 4);
+            addChunk(0, 5);
+            addChunk(0, 6);
+        }
+
+        public void addChunk(int x, int y)
+        {
+            TerrainChunk chunk = new TerrainChunk(Settings, NoiseProvider, x, y);
+            chunk.GenerateHeightmap();
+            chunks.Add(chunk);
+            newestChunk = chunk;
+        }
+
+        private void Update()
+        {
+            foreach (TerrainChunk chunk in chunks)
+            {
+                if (chunk.IsHeightmapReady())
+                {
+                    chunk.CreateTerrain();
+                    chunk.Terrain.transform.position += new Vector3(0, 0, xScrollPosition);
+                }
+            }
+
+            foreach (TerrainChunk chunk in chunks)
+            {
+                if (chunk.Terrain != null)
+                {
+                    chunk.Terrain.transform.position += new Vector3(0, 0, -4);
+                }
+            }
+
+            xScrollPosition += -4;
+            newScrollCounter += 4;
+
+            if (newScrollCounter >= Settings.Length)
+            {
+                newScrollCounter = 0;
+                addChunk(newestChunk.Position.X, newestChunk.Position.Z+1);
+            }
+
+            /*List<TerrainChunk> toDelete = new List<TerrainChunk>();
+
+            foreach (TerrainChunk chunk in chunks)
+            {
+                if (chunk.Terrain != null) {
+                    chunk.Terrain.transform.position += new Vector3(0, 0, -10);
+
+                    if (chunk.Terrain.transform.position.z <= 0 && currentChunk == chunk)
+                    {
+                        addChunk(chunk.Position.X, chunk.Position.Z + 1);
+                    }
+
+                    if (chunk.Terrain.transform.position.z <= -Settings.Length)
+                    {
+                        toDelete.Add(chunk);
+                    }
+                }
+            }
+
+            foreach (TerrainChunk chunk in toDelete)
+            {
+                chunks.Remove(chunk);
+                GameObject.Destroy(chunk.Terrain);
+            }*/
+        }
+    }
+}
